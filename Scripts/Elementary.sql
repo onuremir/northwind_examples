6096a83997e5d54e8419779ac11e1ca153384885
onuremir <emironur22@gmail.com>
2021-10-20 12:02:49 +0300

    updated

--- Scripts/Elementary.sql
+++ Scripts/Elementary.sql
@@ -1,1 +1,28 @@
-SELECT * FROM Customer;
+--1 All shippers
+SELECT * FROM Shipper;
+
+--2 
+SELECT CategoryName, Description FROM Category;
+
+--3 
+SELECT 
+	FirstName, LastName,  HireDate  
+FROM 
+	Employee
+WHERE 
+	Title == 'Sales Representative';
+	
+
+--4
+SELECT 
+	FirstName, LastName,  HireDate  
+FROM 
+	Employee
+WHERE 
+	Title == 'Sales Representative'
+	AND Country == 'USA';
+
+
+--5
+
+
